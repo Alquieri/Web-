9105e7cf463c2080ff9d300fb8956d7cd04367d4
Alquieri <Guilherme.alquieri@gmail.com>
2025-04-09 21:30:10 -0300

    Implementando o dataAnnotatiosn

--- Model/Personagem.cs
+++ Model/Personagem.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Xablau.Models
 {
     public class Personagem
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres!")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O tipo é obrigatório!")]
+        [MaxLength(30, ErrorMessage = "O tipo deve ter no máximo 30 caracteres!")]
         public string Tipo { get; set; }
         
     }
